7be9e5628436dab456b40c782acc61bf4a1c4d4b
surfer1011 <gchacka@gmail.com>
2021-01-21 01:30:35 -0800

    app.py

---
+++ static/app.py
@@ -0,0 +1,33 @@
+
+from flask import Flask, request, redirect, render_template, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+# from flask_wtf import FlaskForm
+# from flask_mail import Message, Mail
+# from threading import Thread
+
+# from models import db, connect_db, User, encrypt_password
+# from forms import RegisterForm, LoginForm, ForgotPasswordForm, EditUserForm, ResetPasswordForm, AdminForm, BanAppealForm
+# # from pws import admin_pw, email_pw
+# from time import time
+import os
+
+
+app = Flask(__name__)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+#     'DATABASE_URL', 'postgres:///flask-heroku')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///saultycraft'#DEVELOPMENT
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# app.config['SQLALCHEMY_ECHO'] = True
+# use secret key in production or default to our dev one
+app.config['SECRET_KEY'] = 'verysecret'
+# app.config['SECRET_KEY'] = admin_pw #DEVELOPMENT
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False#DEVELOPMENT
+
+
+debug = DebugToolbarExtension(app)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
